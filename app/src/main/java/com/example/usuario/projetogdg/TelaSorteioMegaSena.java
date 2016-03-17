72772ef1ebee3eaceb84d50cae635a44220a37a9
Luis Maciel <ikemaciel@hotmail.com>
2016-03-17 14:36:08 -0300

    ListView funcionando

--- app/src/main/java/com/example/usuario/projetogdg/TelaSorteioMegaSena.java
+++ app/src/main/java/com/example/usuario/projetogdg/TelaSorteioMegaSena.java
@@ -3,25 +3,35 @@ package com.example.usuario.projetogdg;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
-
 import java.util.Arrays;
 import java.util.Random;
 
-public class TelaSorteioMegaSena extends AppCompatActivity {
+public class TelaSorteioMegaSena extends AppCompatActivity implements View.OnClickListener {
 
-    private ListView sorteio_list_view;
-    private Button gerarSorteio;
+    private ListView listaSorteio;
+    private ImageButton gerarSorteio;
+    private ArrayAdapter<String> adpLista;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_sorteio_mega_sena);
+
+        gerarSorteio = (ImageButton) findViewById(R.id.gerarSorteio);
+        listaSorteio = (ListView) findViewById(R.id.sorteios_list_view);
+
+        gerarSorteio.setOnClickListener(this);
+
+        adpLista = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
+        listaSorteio.setAdapter(adpLista);
     }
 
-    public void novoSorteio(View view) {
+    @Override
+    public void onClick(View v) {
 
         TextView num1 = (TextView) findViewById(R.id.num1_text_view);
         TextView num2 = (TextView) findViewById(R.id.num2_text_view);
@@ -32,24 +42,23 @@ public class TelaSorteioMegaSena extends AppCompatActivity {
 
         Random r = new Random();
 
-        int[] sorteio = new int[6];
+        final int[] sorteio = new int[6];
 
-        for(int i = 0; i < 6;){
+        for (int i = 0; i < 6; ) {
 
             boolean encontrou = false;
 
             int valor = r.nextInt(60) + 1;
 
-            for(int j = 0; j < sorteio.length; j++){
+            for (int j = 0; j < sorteio.length; j++) {
 
-                // Verifica se tem valores repetidos
-                if(sorteio[j] == valor){
+                if (sorteio[j] == valor) {
                     encontrou = true;
                     break;
                 }
             }
 
-            if(encontrou == false){
+            if (encontrou == false) {
                 sorteio[i] = valor;
                 i++;
             }
@@ -63,5 +72,13 @@ public class TelaSorteioMegaSena extends AppCompatActivity {
         num4.setText(String.valueOf(sorteio[3]));
         num5.setText(String.valueOf(sorteio[4]));
         num6.setText(String.valueOf(sorteio[5]));
+
+        String itemLista = "";
+
+        for (int i = 0; i < sorteio.length; i++) {
+            itemLista += sorteio[i] + "   ";
+        }
+
+        adpLista.add(itemLista);
     }
 }
